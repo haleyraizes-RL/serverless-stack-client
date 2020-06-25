ec19f24c9e57501b27de6a8e9e7dac59c7605be6
haleyraizes-RL <67294271+haleyraizes-RL@users.noreply.github.com>
2020-06-25 11:48:16 -0700

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "us-east-2",
+      BUCKET: "haley-notes-app-uploads"
+    },
+    apiGateway: {
+      REGION: "us-east-2",
+      URL: "https://4b9f5gylf2.execute-api.us-east-2.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "us-east-2",
+      USER_POOL_ID: "us-east-2_TDl4ZwIyA",
+      APP_CLIENT_ID: "653ihnt61al1p7nafs2ibmm23l",
+      IDENTITY_POOL_ID: "us-east-2:dd8579f7-e8de-479b-9d17-7ea2b85b467e"
+    }
+  };
